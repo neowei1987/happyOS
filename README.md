1615ac8f90767ab8b2ce6b7e9e020f7b2f65ebb5
neowei1987 <linfeng303@126.com>
2022-12-28 00:26:43 +0800

    通过修改页表来控制内存映射

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# happyOS
+
+
+freedos中的debug无法调试保护模式，那如何办呢？
+
+借助bochs的debug能力，通过xchg bx, bx指令可以让我们的程序中断在bochs的调试环境中
+
+### 分页机制
+
+bochs.out中有很多有用的信息
+
+必须先关闭分页，再关闭分段；否则无法回到实模式
+
--- booloader_v2.asm
+++ booloader_v2.asm
@@ -2,21 +2,12 @@
 
 org     0100h ; 0x7c00
 
-xchg bx, bx 
+;xchg bx, bx 
 jmp Label_Start
 
 BaseofStack     equ     0x0100 ; 0100h??
-
-PageDirBase0 	equ 200000h ; 页目录开始地址 2M
-PageTblBase0  equ 201000h ;页表开始地址 2M +  4K
-PageDirBase1 	equ 210000h ; 页目录开始地址 2M + 64K 
-PageTblBase1  equ 211000h ;页表开始地址	2M + 64K+ 4K 
-
-LinearAddrDemo	equ	00401000h
-ProcFoo		equ	00401000h
-ProcBar		equ	00501000h
-
-ProcPagingDemo	equ	00301000h
+PageDirBase 	equ 200000h ; 页目录开始地址 2M
+PageTblBase equ 201000h ;页表开始地址
 
 [SECTION .s16]
 [BITS 16]
@@ -224,7 +215,6 @@ LABEL_REAL_ENTRY:
 	and al, 11111101b ;关闭A20地址线
 
 	sti ;开中断
-
 	mov ax, 4c00h 
 	int 21h ;回到DOS ？ 不需要CR0吗？
 
@@ -266,9 +256,7 @@ LABEL_SEG_CODE32:
 
 	call DispMemSize
 
-	call PagingDemo
-
-	;jmp $
+	call SetupPaging
 
 	; 加载TSS，为特权级转换做准备
 	mov ax, SelectorTSS
@@ -345,27 +333,29 @@ SetupPaging:
 	jz .no_remainder
 	inc ecx 
 .no_remainder:
-	;push ecx 
-	mov	[PageTableNumber], ecx	; 暂存页表个数
+	push ecx 
 
 	;初始化页目录
-	mov ax, SelectorFlatRW
+	mov ax, SelectorPageDir
 	mov es, ax 
-	mov edi, PageDirBase0
+	;mov ecx, 1024
+	xor edi, edi 
 	xor eax, eax 
-	mov eax, PageTblBase0 | PG_P | PG_USU | PG_RWW
+	mov eax, PageTblBase | PG_P | PG_USU | PG_RWW
 .4: 
 	stosd 
 	add eax, 4096 
 	loop .4
 	; PageDir的每一项，分别执行每一个TblBase
 
-	;pop eax  ; 页表个数
-	mov	eax, [PageTableNumber]	; 页表个数
+	;初始化页表, 1K个， 4M的空间
+	mov ax, SelectorPageTbl
+	mov es, ax 
+	pop eax  ; 页表个数
 	mov ebx, 1024
 	mul ebx 
 	mov ecx, eax  ; PTE个数 = 页表个数 * 1024
-	mov edi,PageTblBase0
+	xor edi, edi 
 	xor eax, eax 
 	mov eax, PG_P | PG_USU | PG_RWW
 .2: 
@@ -374,7 +364,7 @@ SetupPaging:
 	loop .2
 
 	; 把页目录表基地址放入Cr3
-	mov eax, PageDirBase0
+	mov eax, PageDirBase
 	mov cr3, eax 
 	;设置CR0 的 PG（第232行到第234行），这样，分页机制就启动完成了。
 	mov eax, cr0 
@@ -385,128 +375,7 @@ SetupPaging:
 	nop 
 	ret
 
-PagingDemo:
-	mov ax, cs 
-	mov ds, ax 
-	mov ax, SelectorFlatRW
-	mov es, ax 
-
-	push LenFoo
-	push OffsetFoo
-	push ProcFoo
-	call MemCpy
-	add esp, 12 
-
-	push LenBar 
-	push OffsetBar
-	push ProcBar
-	call MemCpy
-	add esp, 12 
-
-	push LenPagingDemoAll
-	push OffsetPagingDemoProc
-	push ProcPagingDemo
-	call MemCpy
-	add esp, 12 
-
-	mov ax, SelectorData 
-	mov ds, ax
-	mov es, ax 
-
-	call SetupPaging
-
-	call SelectorFlatC:ProcPagingDemo
-	call PSwitch
-	call SelectorFlatC:ProcPagingDemo
-
-	ret
-
-PSwitch:
-	; 首先初始化页目录
-	mov	ax, SelectorFlatRW
-	mov	es, ax
-	mov	edi, PageDirBase1	; 此段首地址为 PageDirBase1
-	xor	eax, eax
-	mov	eax, PageTblBase1 | PG_P  | PG_USU | PG_RWW
-	mov	ecx, [PageTableNumber]
-.1:
-	stosd
-	add	eax, 4096		; 为了简化, 所有页表在内存中是连续的.
-	loop	.1
-
-	; 再初始化所有页表
-	mov	eax, [PageTableNumber]	; 页表个数
-	mov	ebx, 1024		; 每个页表 1024 个 PTE
-	mul	ebx
-	mov	ecx, eax		; PTE个数 = 页表个数 * 1024
-	mov	edi, PageTblBase1	; 此段首地址为 PageTblBase
-	xor	eax, eax
-	mov	eax, PG_P  | PG_USU | PG_RWW
-.2:
-	stosd
-	add	eax, 4096		; 每一页指向 4K 的空间
-	loop	.2
-
-	; 修正
-	mov eax, LinearAddrDemo
-	shr eax, 22 
-	mov ebx, 4096 ; 页目录上的一项，是4K个字节
-	mul ebx
-	mov ecx, eax 
-	mov eax, LinearAddrDemo
-	shr eax, 12
-	and eax, 03FFh 
-	mov ebx, 4 ; 一项是四个字节！
-	mul ebx 
-	add eax, ecx 
-	add eax, PageTblBase1
-	mov dword[es:eax], ProcBar | PG_P | PG_USU | PG_RWW
-	;上面的过程计算，LinearAddrDemo在页表中的位置(物理地址)
-	; ProcBar也是物理地址，讲过上面的操作实现了：让LinearAddrDemo（线性地址）指向了物理地址（ProcBar）
-
-	mov	eax, PageDirBase1
-	mov	cr3, eax
-	jmp	short .3
-.3:
-	nop
-	ret
 %include "lib.inc"
-
-; PagingDemoProc ------------------------------------------------------------
-PagingDemoProc:
-OffsetPagingDemoProc	equ	PagingDemoProc - $$
-	mov	eax, LinearAddrDemo
-	call	eax
-	retf ; 为啥retf??
-; ---------------------------------------------------------------------------
-LenPagingDemoAll	equ	$ - PagingDemoProc
-; ---------------------------------------------------------------------------
-
-; foo -----------------------------------------------------------------------
-foo:
-OffsetFoo	equ	foo - $$
-	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
-	mov	al, 'F'
-	mov	[gs:((80 * 17 + 0) * 2)], ax	; 屏幕第 17 行, 第 0 列。
-	mov	al, 'o'
-	mov	[gs:((80 * 17 + 1) * 2)], ax	; 屏幕第 17 行, 第 1 列。
-	mov	[gs:((80 * 17 + 2) * 2)], ax	; 屏幕第 17 行, 第 2 列。
-	ret
-LenFoo	equ	$ - foo
-
-;bar ---------------------------------------------------------------------------------------------
-bar:
-OffsetBar equ bar - $$ 
-	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
-	mov	al, 'B'
-	mov	[gs:((80 * 18 + 0) * 2)], ax	; 屏幕第 18 行, 第 0 列。
-	mov	al, 'a'
-	mov	[gs:((80 * 18 + 1) * 2)], ax	; 屏幕第 18 行, 第 1 列。
-	mov	al, 'r'
-	mov	[gs:((80 * 18 + 2) * 2)], ax	; 屏幕第 18 行, 第 2 列。
-	ret
-LenBar	equ	$ - bar
-; ---------------------------------------------------------------------------------------------
 
 SegCode32Len equ $-LABEL_SEG_CODE32
 
@@ -534,13 +403,8 @@ LABEL_SEG_CODE16:
 	;check_CR0(0xe0000010): attempt to set CR0.PG with CR0.PE cleared 
 	;答案：mov cr0，ax这句，尝试在pe=0的情况下，设置pg，显然是失败的。
 	;因此，书中的代码实际上是不对的，必须先关闭分页机制，然后再关闭分段机制。
+	jmp $
 
-	;关闭分页
-	mov eax, cr0
-	and eax, 7FFFFFFFh 
-	mov cr0, eax
-
-	;关闭分段
 	mov eax, cr0
 	and al, 11111110b 
 	mov cr0, eax 
@@ -667,7 +531,7 @@ TopOfStack3 equ $ - LABEL_STACK3 - 1 ;？ 为啥有的-1，有的不减呢？
 
 LABEL_GDT: 			Descriptor 	0, 			0, 						0
 LABEL_DESC_NORMAL: 	Descriptor	0, 			0ffffh, 				DA_DRW
-LABEL_DESC_CODE32: 	Descriptor 	0, 			SegCode32Len - 1, 		DA_CR + DA_32 ;非一致代码段 & 可以读取【因为你需要从代码段拷贝数据，所以代码段可读】
+LABEL_DESC_CODE32: 	Descriptor 	0, 			SegCode32Len - 1, 		DA_C + DA_32 ;非一致代码段 
 LABEL_DESC_CODE16: 	Descriptor 	0, 			0ffffh, 				DA_C		 ;非一致代码段 
 LABEL_DESC_CODE_CALL: Descriptor 0, 		SegCodeDestLen - 1, DA_C + DA_32  ;非一致代码段
 LABEL_DESC_CODE_RING3: Descriptor 0, 		SegCodeRing3Len - 1, DA_C + DA_32 + DA_DPL3 ;非一致代码段
@@ -683,11 +547,8 @@ LABEL_DESC_TSS: 	Descriptor 	0, 	TSSLen - 1, DA_386TSS
 ; 门
 LABEL_CALL_GATE_TEST: Gate Selector_CODE_CALL, 0, 0, DA_386CGate + DA_DPL3 
 
-;LABEL_DESC_PAGE_DIR: Descriptor PageDirBase, 4096, DA_DRW 
-;LABEL_DESC_PAGE_TBL: Descriptor PageTblBase, 8 * 4096, DA_DRW; 为啥去掉这个flag了？?+ DA_LIMIT_4K
-
-LABEL_DESC_FLAT_C Descriptor 0, 0fffffh, DA_CR | DA_32 | DA_LIMIT_4K
-LABEL_DESC_FLAT_RW Descriptor 0, 0fffffh, DA_DRW | DA_LIMIT_4K
+LABEL_DESC_PAGE_DIR: Descriptor PageDirBase, 4096, DA_DRW 
+LABEL_DESC_PAGE_TBL: Descriptor PageTblBase, 8 * 4096, DA_DRW; 为啥去掉这个flag了？?+ DA_LIMIT_4K
 
 GdtLen	equ $ - LABEL_GDT
 
@@ -708,12 +569,8 @@ SelectorTest 	equ LABEL_DESC_TEST - LABEL_GDT
 SelectorLDT 	equ LABEL_DESC_LDT - LABEL_GDT 
 SelectorCallGateTest equ LABEL_CALL_GATE_TEST - LABEL_GDT + SA_RPL3
 SelectorTSS 	equ LABEL_DESC_TSS - LABEL_GDT
-;SelectorPageDir equ LABEL_DESC_PAGE_DIR - LABEL_GDT
-;SelectorPageTbl equ LABEL_DESC_PAGE_TBL - LABEL_GDT 
-
-SelectorFlatC 	equ LABEL_DESC_FLAT_C - LABEL_GDT
-SelectorFlatRW 	equ LABEL_DESC_FLAT_RW - LABEL_GDT
-
+SelectorPageDir equ LABEL_DESC_PAGE_DIR - LABEL_GDT
+SelectorPageTbl equ LABEL_DESC_PAGE_TBL - LABEL_GDT 
 
 ;END of [SECTION .gdt]
 
@@ -752,8 +609,6 @@ _ARDStruct: 	; Address Range Descriptor Structure
 	_ddType: dd 0
 PModeMessage:	db "In ProtectMode now...", 0Ah, 0
 StrTest:	db "ABCDEFGHIJKLMNOPORSTUVWXYZ", 0
-_PageTableNum dd 0 
-
 
 OffsetPModeMessage 	equ PModeMessage - $$
 OffsetStrTest equ StrTest - $$
@@ -770,9 +625,8 @@ ARDStruct equ	_ARDStruct	- $$
 dwDispPos		equ	_dwDispPos	- $$
 dwMemSize		equ	_dwMemSize	- $$
 dwMCRNumber		equ	_dwMCRNumber	- $$
-PageTableNumber equ _PageTableNum - $$
 
-DataLen equ $ -LABEL_DATA
+DataLen equ $-LABEL_DATA
 
 ; 全局堆栈段
 [SECTION .gs]
